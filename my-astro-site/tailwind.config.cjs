4bf0172b2ffba7c8df6b9cd75bc205916a789d9d
Milos Danilovic <danilovicmilos25@gmail.com>
2022-11-27 17:48:49 +0100

    Made a temp header component and startet implementing light and dark theme

--- my-astro-site/tailwind.config.cjs
+++ my-astro-site/tailwind.config.cjs
@@ -1,15 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+		colors: {
+			transparent: 'transparent',
+			current: 'currentColor',
+			'white': 'var(--lowContrastText)',
+			'purple': 'var(--mainBackground)',
+			'midnight': '#121063',
+			'bla': 'var(--testNameBackground)',
+			'tahiti': '#3ab7bf',
+			'silver': '#ecebff',
+			'bubble-gum': '#ff77e9',
+			'bermuda': '#78dcca',
+		  },
 	},
-	plugins: [require("daisyui")],
-	daisyui: {
-		styled: true,
-		themes: ["light"],
-		base: true,
-		utils: true,
-	
-	  },
-}
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: [],
+    base: true,
+    utils: true,
+  },
+};
