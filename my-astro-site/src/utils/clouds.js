890ee9335c965d15dd2bb868d7c103fed5e03508
Milos Danilovic <danilovicmilos25@gmail.com>
2022-12-26 22:28:35 +0100

    Added clouds animations

---
+++ my-astro-site/src/utils/clouds.js
@@ -0,0 +1,31 @@
+import cloudImage from "../public/images/kkk.svg";
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+export const kill = (rootElement, interval,dencity) => {
+    
+  
+  setTimeout(() => {
+    for (let index = 0; index < dencity; index++) {
+      const cloud = document.createElement("img");
+      cloud.src = cloudImage;
+
+      let topValue = getRandomIntInclusive(0, rootElement.offsetHeight-120);
+      let leftValue = getRandomIntInclusive(0, -3000);
+
+     console.log(rootElement.childNodes);
+        setTimeout(()=>{
+            rootElement.childNodes
+        },interval*10)
+      cloud.classList.add("cloud");
+      cloud.style.top = `${topValue}px`;
+      cloud.style.left = `${leftValue}px`;
+
+      rootElement.append(cloud);
+    }
+  }, interval);
+};
