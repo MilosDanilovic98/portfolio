a0339e47b6f59344029e89c018181ffd7fc30738
Milos Danilovic <danilovicmilos25@gmail.com>
2023-01-14 17:15:01 +0100

    finished clouds animations

--- my-astro-site/src/utils/clouds.js
+++ my-astro-site/src/utils/clouds.js
@@ -6,26 +6,45 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
-export const kill = (rootElement, interval,dencity) => {
-    
-  
+export const kill = (rootElement, interval, dencity) => {
   setTimeout(() => {
+    let margin = 200;
+
     for (let index = 0; index < dencity; index++) {
       const cloud = document.createElement("img");
       cloud.src = cloudImage;
-
-      let topValue = getRandomIntInclusive(0, rootElement.offsetHeight-120);
-      let leftValue = getRandomIntInclusive(0, -3000);
-
-     console.log(rootElement.childNodes);
-        setTimeout(()=>{
-            rootElement.childNodes
-        },interval*10)
+      let topValue = getRandomIntInclusive(0, rootElement.offsetHeight - 120);
+      let leftValue = getRandomIntInclusive(
+        rootElement.offsetWidth - margin,
+        rootElement.offsetWidth + 400
+      );
+      margin -= 200;
       cloud.classList.add("cloud");
       cloud.style.top = `${topValue}px`;
       cloud.style.left = `${leftValue}px`;
 
       rootElement.append(cloud);
+
+      const moveLeft = (timestamp) => {
+        cloud.style.left = `${cloud.getBoundingClientRect().left - 3}px`;
+        if (cloud.getBoundingClientRect().right < 0) {
+          let newtop = `${getRandomIntInclusive(
+            0,
+            rootElement.offsetHeight - 120
+          )}px`;
+
+          let newLeft = `${getRandomIntInclusive(
+            rootElement.offsetWidth-margin,
+            rootElement.offsetWidth + 400
+          )}px`;
+
+          cloud.style.top = newtop;
+          cloud.style.left = newLeft;
+        }
+        window.requestAnimationFrame(moveLeft);
+      };
+
+      window.requestAnimationFrame(moveLeft);
     }
   }, interval);
 };
