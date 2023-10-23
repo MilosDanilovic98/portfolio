2a91ada94ad4dcc7516dc81b8b31710a53c34ed6
MilosDanilovic98 <danilovicmilos25@gmail.com>
2023-10-23 13:08:31 +0200

    fix(cloud-animations):Configuring animations

--- my-astro-site/src/utils/clouds.js
+++ my-astro-site/src/utils/clouds.js
@@ -10,35 +10,39 @@ function getRandomIntInclusive(min, max) {
 export const kill = (rootElement, interval, dencity,speed) => {
 
 
-  for (let index = 0; index < dencity; index++) {
-    const cloud = document.createElement("img");
-    cloud.src = cloudImage;
-    let topValue = getRandomIntInclusive(0, rootElement.offsetHeight - 120);
-    let rightValue = getRandomIntInclusive(
-        -200,
-        -1600
-    );
+  const createClouds=()=>{
+    for (let index = 0; index < dencity; index++) {
+      const cloud = document.createElement("img");
+      cloud.src = cloudImage;
+      let topValue = getRandomIntInclusive(0, rootElement.offsetHeight - 120);
+      let rightValue = getRandomIntInclusive(
+          -200,
+          -1600
+      );
+
+      cloud.classList.add("cloud");
+      cloud.style.top = `${topValue}px`;
+      cloud.style.right = `${rightValue}px`;
+      cloud.style.scale=Math.random() * (1.4 - 0.7) + 0.7;
+
+      rootElement.append(cloud);
+      let leftValue = getRandomIntInclusive(
+          -400,
+          -1000
+      );
+
+      gsap.to(cloud,{left:leftValue,duration:speed, onComplete: ()=>cloud.remove(),force3D: true})
 
 
-
-
-
-    cloud.classList.add("cloud");
-    cloud.style.top = `${topValue}px`;
-    cloud.style.right = `${rightValue}px`;
-    cloud.style.scale=Math.random() * (1.4 - 0.7) + 0.7;
-
-    rootElement.append(cloud);
-    let leftValue = getRandomIntInclusive(
-        -400,
-        -1000
-    );
-
-
-
-    gsap.to(cloud,{left:leftValue,duration:speed, onComplete: ()=>cloud.remove(),})
+    }
 
   }
+
+
+  createClouds()
+
+
+
 
 
 
@@ -47,41 +51,8 @@ export const kill = (rootElement, interval, dencity,speed) => {
   setInterval(() => {
 
     if (!document.hidden) {
-
-
-    for (let index = 0; index < dencity; index++) {
-      const cloud = document.createElement("img");
-      cloud.src = cloudImage;
-      let topValue = getRandomIntInclusive(0, rootElement.offsetHeight - 120);
-      let rightValue = getRandomIntInclusive(
-        -200,
-          -1600
-      );
-
-      let cloudScale = getRandomIntInclusive(
-          0,
-          2
-      );
-
-
-
-      cloud.classList.add("cloud");
-      cloud.style.top = `${topValue}px`;
-
-      cloud.style.right = `${rightValue}px`;
-      cloud.style.scale=Math.random() * (1.4 - 0.7) + 0.7;
-
-
-      rootElement.append(cloud);
-      let leftValue = getRandomIntInclusive(
-          -400,
-          -1000
-      );
-
-
-      gsap.to(cloud,{left:leftValue,duration:speed, onComplete: ()=>cloud.remove(),})
-
+      createClouds()
     }
-    }
+
   }, interval);
 };
