b902e4d3700ada5e60a71fcbf8a130f4a72cd501
MilosDanilovic98 <danilovicmilos25@gmail.com>
2024-01-31 11:15:13 +0100

    chore(general):remove console logs

--- my-astro-site/src/utils/clouds.js
+++ my-astro-site/src/utils/clouds.js
@@ -17,7 +17,7 @@ export const cloudGeneratingFunction = (rootElement, interval, dencity,speed) =>
 
   changeThemeButton.addEventListener("click", (e) => {
     if (!e.target.checked) {
-      console.log("test2")
+
       createClouds()
       for (let i = 0; i < cloudAnimations.length; i++) {
         cloudAnimations[i].play()
